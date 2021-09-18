3ee408c1209ca6d6e5e7001ffe5a558b9b083387
Alexander <alexanderfsp@mail.ru>
2021-09-18 23:22:28 +0300

    Setup tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
